c5a0132699d0b7d791d5c4a6566e977660879690
Lambdalf the White <silvereh@protonmail.com>
2022-04-29 12:43:20 -0600

    Added tests for IInitializable

---
+++ test/utils/IInitializable.test.js
@@ -0,0 +1,184 @@
+const ARTIFACT = require( `../../artifacts/contracts/mocks/utils/Mock_IInitializable.sol/Mock_IInitializable.json` )
+// **************************************
+// *****           IMPORT           *****
+// **************************************
+	const { TEST_ACTIVATION } = require( `../test-activation-module` )
+	const {
+		CST,
+		USER1,
+		USER2,
+		USER_NAMES,
+		PROXY_USER,
+		TOKEN_OWNER,
+		OTHER_OWNER,
+		CONTRACT_DEPLOYER,
+	} = require( `../test-var-module` )
+
+	const chai = require( `chai` )
+	const chaiAsPromised = require( `chai-as-promised` )
+	chai.use( chaiAsPromised )
+	const expect = chai.expect
+
+	const { ethers, waffle } = require( `hardhat` )
+	const { loadFixture, deployContract } = waffle
+
+	const {
+		getTestCasesByFunction,
+		generateTestCase
+	} = require( `../fail-test-module` )
+
+	const {
+		shouldRevertWhenAlreadyInitialized,
+	} = require( `../utils/behavior.IInitializable` )
+// **************************************
+
+// **************************************
+// *****       TEST VARIABLES       *****
+// **************************************
+	// For contract data
+	const CONTRACT = {
+		NAME : `Mock_IInitializable`,
+		METHODS : {
+			initialize  : {
+				SIGNATURE : `initialize()`,
+				PARAMS    : [],
+			},
+		},
+	}
+
+	const TEST_DATA = {
+		NAME : `IInitializable`,
+		METHODS : {
+			initialize : true,
+		}
+	}
+
+	let users = {}
+	let contract
+// **************************************
+
+// **************************************
+// *****          FIXTURES          *****
+// **************************************
+	async function fixture () {
+		const [
+			test_user1,
+			test_user2,
+			test_proxy_user,
+			test_token_owner,
+			test_other_owner,
+			test_contract_deployer,
+			...addrs
+		] = await ethers.getSigners()
+
+		test_contract_params = [
+		]
+		test_contract = await deployContract( test_contract_deployer, ARTIFACT, test_contract_params )
+		await test_contract.deployed()
+
+		return {
+			test_user1,
+			test_user2,
+			test_contract,
+			test_proxy_user,
+			test_token_owner,
+			test_other_owner,
+			test_contract_deployer,
+		}
+	}
+// **************************************
+
+// **************************************
+// *****        TEST  SUITES        *****
+// **************************************
+	function testInvalidInputs ( fixture, TEST, CONTRACT ) {
+		describe( `Invalid inputs`, function () {
+			if ( TEST_ACTIVATION.INVALID_INPUT ) {
+				beforeEach( async function () {
+					const {
+						test_user1,
+						test_user2,
+						test_contract,
+						test_proxy_user,
+						test_token_owner,
+						test_other_owner,
+						test_contract_deployer,
+					} = await loadFixture( fixture )
+
+					contract       = test_contract
+					users[ USER1             ] = test_user1
+					users[ USER2             ] = test_user2
+					users[ PROXY_USER        ] = test_proxy_user
+					users[ TOKEN_OWNER       ] = test_token_owner
+					users[ OTHER_OWNER       ] = test_other_owner
+					users[ CONTRACT_DEPLOYER ] = test_contract_deployer
+
+					defaultArgs = {}
+					defaultArgs [ CONTRACT.METHODS.initialize.SIGNATURE ] = {
+						err  : null,
+						args : [
+						]
+					}
+				})
+
+				Object.entries( CONTRACT.METHODS ).forEach( function( [ prop, val ] ) {
+					describe( val.SIGNATURE, function () {
+						const testSuite = getTestCasesByFunction( val.SIGNATURE, val.PARAMS )
+
+						testSuite.forEach( testCase => {
+							it( testCase.test_description, async function () {
+								await generateTestCase( contract, testCase, defaultArgs, prop, val )
+							})
+						})
+					})
+				})
+			}
+		})
+	}
+
+	async function shouldBehaveLikeMock_IInitializable ( fixture, TEST, CONTRACT ) {
+		describe( `Should behave like IInitializable`, function () {
+			if ( TEST_ACTIVATION.CORRECT_INPUT ) {
+				beforeEach( async function () {
+					const {
+						test_user1,
+						test_user2,
+						test_contract,
+						test_proxy_user,
+						test_token_owner,
+						test_other_owner,
+						test_contract_deployer,
+					} = await loadFixture( fixture )
+
+					contract       = test_contract
+					users[ USER1             ] = test_user1
+					users[ USER2             ] = test_user2
+					users[ PROXY_USER        ] = test_proxy_user
+					users[ TOKEN_OWNER       ] = test_token_owner
+					users[ OTHER_OWNER       ] = test_other_owner
+					users[ CONTRACT_DEPLOYER ] = test_contract_deployer
+				})
+
+				describe( CONTRACT.METHODS.initialize.SIGNATURE, function () {
+					if ( TEST.METHODS.initialize ) {
+						it( `Should revert when already initialized`, async function () {
+							await shouldRevertWhenAlreadyInitialized(
+								contract.initialize()
+							)
+						})
+					}
+				})
+			}
+		})
+	}
+// **************************************
+
+// **************************************
+// *****          TEST RUN          *****
+// **************************************
+describe( TEST_DATA.NAME, function () {
+	if ( TEST_ACTIVATION[ TEST_DATA.NAME ] ) {
+		testInvalidInputs( fixture, TEST_DATA, CONTRACT )
+		shouldBehaveLikeMock_IInitializable( fixture, TEST_DATA, CONTRACT )
+	}
+})
---
+++ test/utils/behavior.IInitializable.js
@@ -0,0 +1,50 @@
+// **************************************
+// *****           IMPORT           *****
+// **************************************
+	const { TEST_ACTIVATION } = require( `../test-activation-module` )
+	const {
+		CST,
+		USER1,
+		USER2,
+		USER_NAMES,
+		PROXY_USER,
+		TOKEN_OWNER,
+		OTHER_OWNER,
+		CONTRACT_DEPLOYER,
+	} = require( `../test-var-module` )
+
+	const chai = require( `chai` )
+	const chaiAsPromised = require( `chai-as-promised` )
+	chai.use( chaiAsPromised )
+	const expect = chai.expect
+
+	const { ethers, waffle } = require( `hardhat` )
+	const { loadFixture, deployContract } = waffle
+
+	const {
+		getTestCasesByFunction,
+		generateTestCase
+	} = require( `../fail-test-module` )
+// **************************************
+
+// **************************************
+// *****       TEST VARIABLES       *****
+// **************************************
+	let contract
+	let users = {}
+// **************************************
+
+// **************************************
+// *****        TEST  SUITES        *****
+// **************************************
+	async function shouldRevertWhenAlreadyInitialized ( promise, error = `IInitializable_ALREADY_INITIALIZED` ) {
+		await expect( promise ).to.be.revertedWith( `${ error }()` )
+	}
+// **************************************
+
+// **************************************
+// *****           EXPORT           *****
+// **************************************
+module.exports = {
+	shouldRevertWhenAlreadyInitialized,
+}
