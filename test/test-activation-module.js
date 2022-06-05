6d4634e014080179169cba66fc69e09798509925
Lambdalf the White <silvereh@protonmail.com>
2022-06-05 17:38:53 -0600

    Update test-activation-module.js

--- test/test-activation-module.js
+++ test/test-activation-module.js
@@ -1,28 +1,52 @@
 const TEST_ACTIVATION = {
-	CORRECT_INPUT : true,
-	INVALID_INPUT : false,
-	// UTILS
-	ERC2981Base          : true,
-	IInitializable       : true,
-	IOwnable             : true,
-	IPausable            : true,
-	ITradable            : true,
-	IMerkleWhitelistable : true,
-	// ERC721
-	Consec_ERC721Batch         : true,
-	ERC721A                    : false,
-	ERC721B                    : false,
-	ERC721OZ                   : false,
-	Reg_ERC721Batch            : true,
-	// ERC721 EXTENSIONS
-	Consec_ERC721BatchBurnable : true,
-	Reg_ERC721BatchBurnable    : true,
-	// NFT BASE
-	NFTBaseC : true,
-	NFTBaseR : true,
-	// PRICE COMPARISON
-	PRICE_MINT_ERC721     : false,
-	PRICE_TRANSFER_ERC721 : false,
+	// **************************************
+	// *****           INPUTS           *****
+	// **************************************
+		CORRECT_INPUT                 : true,
+		INVALID_INPUT                 : true,
+	// **************************************
+
+	// **************************************
+	// *****            UTIL            *****
+	// **************************************
+		ERC2981Base                   : true,
+		IInitializable                : true,
+		IOwnable                      : true,
+		IPausable                     : true,
+		ITradable                     : true,
+		IMerkleWhitelistable          : true,
+	// **************************************
+
+	// **************************************
+	// *****           ERC721           *****
+	// **************************************
+		Consec_ERC721Batch            : true,
+		ERC721A                       : true,
+		ERC721B                       : true,
+		ERC721OZ                      : true,
+		Reg_ERC721Batch               : true,
+	// **************************************
+
+	// **************************************
+	// *****     ERC721  EXTENSIONS     *****
+	// **************************************
+		Consec_ERC721BatchBurnable    : true,
+		Reg_ERC721BatchBurnable       : true,
+	// **************************************
+
+	// **************************************
+	// *****          NFT BASE          *****
+	// **************************************
+		NFTBaseC                      : true,
+		NFTBaseR                      : true,
+	// **************************************
+
+	// **************************************
+	// *****      PRICE COMPARISON      *****
+	// **************************************
+		PRICE_MINT_ERC721             : true,
+		PRICE_TRANSFER_ERC721         : true,
+	// **************************************
 }
 
 module.exports = {
